1b30f112581d653b59343af153c99656488ae432
김성식 <kss7858@naver.com>
2019-02-13 23:33:30 +0900

    no message

---
+++ Algorithm/TEST_PREPARE/src/TestMain.java
@@ -0,0 +1,15 @@
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 9;
+		for (int j = 0; j < x; j++) {
+		    x = x + y;
+		    System.out.println(x);
+//		    System.out.println(y);
+		}
+//		System.out.println(y);
+		System.out.println(x);
+	}
+}
