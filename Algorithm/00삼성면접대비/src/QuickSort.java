8c88d0f74d9d3375d9e4206860ef9a6d5eebbf18
김성식 <kss7858@naver.com>
2020-03-12 22:32:53 +0900

    Quick sort

--- Algorithm/00삼성면접대비/src/QuickSort.java
+++ Algorithm/00삼성면접대비/src/QuickSort.java
@@ -1,6 +1,5 @@
 
 public class QuickSort {
-	
 	public static int partition(int[] array, int left, int right) {
 		int mid = (left + right ) / 2;
 		swap(array, left, mid);
@@ -9,15 +8,21 @@ public class QuickSort {
 		int i = left, j = right;
 		
 		while(i < j) {
+			
+			// 피벗보다 작은 애를 끝에서 부터 찾아.
 			while(pivot < array[j]) {
 				j--;
 			}
 			
+			// 피벗보다 크거나 같은 애를 시작지점부터 찾아.
 			while(i < j && pivot >= array[i]) {
 				i++;
 			}
+			// 그 둘을 swap
 			swap(array,i,j);
 		}
+		
+		// 마지막의 i자리가 pivot자리가 돼.
 		array[left] = array[i];
 		array[i] = pivot;
 		
@@ -33,16 +38,14 @@ public class QuickSort {
 		}
 	}
 	
-	
 	public static void swap(int[] array, int a, int b) {
 		int temp = array[b];
 		array[b] = array[a];
 		array[a] = temp;
-
 	}
 	
 	public static void quicksort(int[] array, int left, int right) {
-		// 
+		
 		if ( left >= right) {
 			return;
 		}
